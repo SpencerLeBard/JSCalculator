9a8bf472b96175a8bc7973723b481b8d89f5a308
SpencerLeBard <slebard@gmail.com>
2020-12-08 12:28:16 -0700

    almost adding

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 let calculatorDisplay = document.getElementById('calculatorDisplay')
 
-let total = 0
+let total = {
+  string: "",
+  num: 0
+}
 
 function drawDisplay(){
-let template = `${total}`
+let template = `${total.num}`
 calculatorDisplay.innerText = template
-drawDisplay();
 } 
+drawDisplay();
+
+//NOTE 2 draw displays? one for begining one end/strings,nums?
+
+function add(firstNum) {
+  let addition = firstNum += 2
+  total.num = addition 
+  drawDisplay()
+}
+
 
 function clickNumber(digit){
-  total = digit
+  total.string = total.string += digit
+  total.num = Number.parseInt(total.string)
   drawDisplay();
 }
